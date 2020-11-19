7bfc1731b9d422a0c2db7875fa627cb9e8eab079
Paul Bouman <bouman@ese.eur.nl>
2020-11-19 11:56:46 +0100

    Changed build config to easier accomodate GitHub pages

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,10 @@
+const path = require("path");
+
 module.exports = {
   "transpileDependencies": [
     "vuetify"
   ],
+  outputDir: path.resolve(__dirname, "docs"),
   pages: {
     index: {
       entry: './src/main.js',
